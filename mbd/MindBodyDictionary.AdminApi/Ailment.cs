5b7285bdb917b8d93a6759c12e846900289bc189
Austin Talbot <austintalbot@gmail.com>
2025-11-10 23:45:57 -0700

    refactor: Clean up using directives and update .gitignore for local settings

--- mbd/MindBodyDictionary.AdminApi/Ailment.cs
+++ mbd/MindBodyDictionary.AdminApi/Ailment.cs
@@ -1,63 +1,55 @@
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Cosmos;
-using MindBodyDictionary.CosmosDB;
+namespace MindBodyDictionary.AdminApi;
 
-namespace MindBodyDictionary.AdminApi
+public class Ailment
 {
-    public class Ailment
+    private readonly ILogger<Ailment> _logger;
+
+    public Ailment(ILogger<Ailment> logger)
     {
-        private readonly ILogger<Ailment> _logger;
+        _logger = logger;
+    }
 
-        public Ailment(ILogger<Ailment> logger)
+         /// <summary>
+    /// Called to retrieve the full Ailment object based on a query string passed in
+    /// </summary>
+    /// <param name="req"></param>
+    /// <param name="log"></param>
+    /// <param name="ailment"></param>
+    /// <returns>Ailment</returns>
+    [Function("Ailment")]
+    public  async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+        [CosmosDBInput(
+            Connection = Core.CosmosDB.ConnectionStringSetting)] CosmosClient client
+        )
+    {
+        try
         {
-            _logger = logger;
+            var id = req.Query["id"];
+            _logger.LogInformation($" Get Ailment Id = {id}");
+
+            // When we query for a single item it finds results, but fails to get them
+            // We are getting the list so we work around by doing that.  This only works for small
+            // data sets and will not be scalable
+            var item = await client.GetItemAsync<Core.Entities.Ailment>(
+                databaseName: Core.CosmosDB.DatabaseName,
+                containerName: Core.CosmosDB.Containers.Ailments,
+                query: "SELECT * FROM c",
+                itemSelector: x => x.id == id);
+
+            if (item != null)
+            {
+                return new OkObjectResult(item);
+            }
+            else
+            {
+                return new NotFoundResult();
+            }
+        }
+        catch (Exception ex)
+        {
+            return new BadRequestObjectResult(ex.ToString());
         }
 
-             /// <summary>
-        /// Called to retrieve the full Ailment object based on a query string passed in
-        /// </summary>
-        /// <param name="req"></param>
-        /// <param name="log"></param>
-        /// <param name="ailment"></param>
-        /// <returns>Ailment</returns>
-        [Function("Ailment")]
-        public  async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
-            [CosmosDBInput(
-                Connection = Core.CosmosDB.ConnectionStringSetting)] CosmosClient client
-            )
-        {
-            try
-            {
-                var id = req.Query["id"];
-                _logger.LogInformation($" Get Ailment Id = {id}");
-
-                // When we query for a single item it finds results, but fails to get them
-                // We are getting the list so we work around by doing that.  This only works for small
-                // data sets and will not be scalable
-                var item = await client.GetItemAsync<Core.Entities.Ailment>(
-                    databaseName: Core.CosmosDB.DatabaseName,
-                    containerName: Core.CosmosDB.Containers.Ailments,
-                    query: "SELECT * FROM c",
-                    itemSelector: x => x.id == id);
-
-                if (item != null)
-                {
-                    return new OkObjectResult(item);
-                }
-                else
-                {
-                    return new NotFoundResult();
-                }
-            }
-            catch (Exception ex)
-            {
-                return new BadRequestObjectResult(ex.ToString());
-            }
-
-        }
     }
 }
--- mbd/MindBodyDictionary.AdminApi/AilmentsTable.cs
+++ mbd/MindBodyDictionary.AdminApi/AilmentsTable.cs
@@ -1,44 +1,38 @@
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+namespace MindBodyDictionary.AdminApi;
 
-namespace MindBodyDictionary.AdminApi
+public class AilmentsTable
 {
-    public class AilmentsTable
+    private readonly ILogger<AilmentsTable> _logger;
+
+    public AilmentsTable(ILogger<AilmentsTable> logger)
     {
-        private readonly ILogger<AilmentsTable> _logger;
+        _logger = logger;
+    }
 
-        public AilmentsTable(ILogger<AilmentsTable> logger)
+    [Function("AilmentsTable")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req,
+     [CosmosDBInput(  databaseName: Core.CosmosDB.DatabaseName,
+            containerName: Core.CosmosDB.Containers.Ailments,
+            Connection = Core.CosmosDB.ConnectionStringSetting,
+            SqlQuery = "SELECT * FROM Ailments")] IEnumerable<Core.Entities.Ailment> ailments)
+    {
+        _logger.LogInformation("Ailments Table function processed a request.");
+        try
         {
-            _logger = logger;
+            if (ailments is null)
+            {
+                return new NotFoundResult();
+
+            }
+            _logger.LogInformation(message: $"""Found {ailments.Count()} ailments""");
+          
+            var result = new { data = ailments.OrderBy(a => a.Name) };
+            return new JsonResult(result);
         }
-
-        [Function("AilmentsTable")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req,
-         [CosmosDBInput(  databaseName: Core.CosmosDB.DatabaseName,
-                containerName: Core.CosmosDB.Containers.Ailments,
-                Connection = Core.CosmosDB.ConnectionStringSetting,
-                SqlQuery = "SELECT * FROM Ailments")] IEnumerable<Core.Entities.Ailment> ailments)
+        catch (Exception ex)
         {
-            _logger.LogInformation("Ailments Table function processed a request.");
-            try
-            {
-                if (ailments is null)
-                {
-                    return new NotFoundResult();
-
-                }
-                _logger.LogInformation(message: $"""Found {ailments.Count()} ailments""");
-              
-                var result = new { data = ailments.OrderBy(a => a.Name) };
-                return new JsonResult(result);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error getting ailments");
-                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
-            }
+            _logger.LogError(ex, "Error getting ailments");
+            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
         }
     }
 }
