82dba4bf2281d4b261489c8de37cbd77a76981ac
Austin Talbot <austintalbot@gmail.com>
2025-11-11 23:26:02 -0700

    feat: Update function app outputs to use azapi_resource for improved resource management

--- backend/terraform/modules/function_app/outputs.tf
+++ backend/terraform/modules/function_app/outputs.tf
@@ -1,11 +1,11 @@
 output "name" {
-  value = azurerm_linux_function_app.main.name
+  value = azapi_resource.function_app.name
 }
 
 output "id" {
-  value = azurerm_linux_function_app.main.id
+  value = azapi_resource.function_app.id
 }
 
 output "default_hostname" {
-  value = azurerm_linux_function_app.main.default_hostname
+  value = try(jsondecode(azapi_resource.function_app.output).properties.defaultHostName, "")
 }
