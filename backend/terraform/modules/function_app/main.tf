c1135837feb0391ee22ff314ed1bc52c3e07b378
Austin Talbot <austintalbot@gmail.com>
2025-11-11 23:25:55 -0700

    feat: Remove outdated deployment and quick start documentation; update function app configuration for .NET 10 support

--- backend/terraform/modules/function_app/main.tf
+++ backend/terraform/modules/function_app/main.tf
@@ -8,34 +8,48 @@ resource "azurerm_service_plan" "main" {
   tags = var.tags
 }
 
-resource "azurerm_linux_function_app" "main" {
+# Data source for the function app to configure with AzAPI
+data "azurerm_linux_function_app" "main" {
   name                = "${var.project_name}-admin-api"
-  location            = var.location
   resource_group_name = var.resource_group_name
-  service_plan_id     = azurerm_service_plan.main.id
-
-  storage_account_name       = var.storage_account_name
-  storage_account_access_key = var.storage_account_key
-  https_only                 = true
-  functions_extension_version = "~4"
-
-  app_settings = {
-    FUNCTIONS_WORKER_RUNTIME      = "dotnet-isolated"
-    WEBSITE_RUN_FROM_PACKAGE      = "1"
-    WEBSITE_DYNAMIC_CACHE         = "0"
-    WEBSITE_LOCAL_CACHE_OPTION    = "Never"
-    SCM_DO_BUILD_DURING_DEPLOYMENT = "0"
-    CONNECTION_COSMOSDB           = "AccountEndpoint=${var.cosmosdb_endpoint};AccountKey=${var.cosmosdb_key};"
-    CONNECTION_STORAGE            = "BlobEndpoint=https://${var.storage_account_name}.blob.core.windows.net/;QueueEndpoint=https://${var.storage_account_name}.queue.core.windows.net/;FileEndpoint=https://${var.storage_account_name}.file.core.windows.net/;TableEndpoint=https://${var.storage_account_name}.table.core.windows.net/;SharedAccessSignature=sv=2024-05-04"
-  }
-
-  site_config {
-    always_on          = false
-    
-    application_stack {
-      dotnet_version = "10.0"
-    }
-  }
-
-  tags = var.tags
+  
+  depends_on = [azurerm_service_plan.main]
 }
+
+# Function app configuration using AzAPI for .NET 10 support
+# Standard azurerm provider doesn't yet support .NET 10, only up to 8.0
+resource "azapi_resource" "function_app" {
+  type      = "Microsoft.Web/sites@2023-12-01"
+  name      = "${var.project_name}-admin-api"
+  parent_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}"
+  location  = var.location
+
+  body = {
+    properties = {
+      serverFarmId = azurerm_service_plan.main.id
+      httpsOnly    = true
+      siteConfig = {
+        linuxFxVersion              = "DOTNET-ISOLATED|10"
+        alwaysOn                    = false
+        numberOfWorkers             = 1
+        functionsExtensionVersion   = "~4"
+        appSettings = [
+          { name = "FUNCTIONS_WORKER_RUNTIME", value = "dotnet-isolated" },
+          { name = "WEBSITE_RUN_FROM_PACKAGE", value = "1" },
+          { name = "WEBSITE_DYNAMIC_CACHE", value = "0" },
+          { name = "WEBSITE_LOCAL_CACHE_OPTION", value = "Never" },
+          { name = "SCM_DO_BUILD_DURING_DEPLOYMENT", value = "0" },
+          { name = "CONNECTION_COSMOSDB", value = "AccountEndpoint=${var.cosmosdb_endpoint};AccountKey=${var.cosmosdb_key};" },
+          { name = "CONNECTION_STORAGE", value = "BlobEndpoint=https://${var.storage_account_name}.blob.core.windows.net/;QueueEndpoint=https://${var.storage_account_name}.queue.core.windows.net/;FileEndpoint=https://${var.storage_account_name}.file.core.windows.net/;TableEndpoint=https://${var.storage_account_name}.table.core.windows.net/;SharedAccessSignature=sv=2024-05-04" },
+          { name = "AzureWebJobsStorage", value = "DefaultEndpointsProtocol=https;AccountName=${var.storage_account_name};AccountKey=${var.storage_account_key};EndpointSuffix=core.windows.net" }
+        ]
+      }
+    }
+    tags = var.tags
+  }
+
+  depends_on = [azurerm_service_plan.main]
+}
+
+# Data source to retrieve current subscription context
+data "azurerm_client_config" "current" {}
--- mbd/MindBodyDictionary.AdminApi/Extensions.cs
+++ mbd/MindBodyDictionary.AdminApi/Extensions.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
-
+﻿
 namespace MindBodyDictionary.AdminApi
 {
     internal static class Extensions
